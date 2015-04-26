daff62dc82e7cba4f7fcdd18b0dcfada2d176e01
ThomasSpanberger <thomasspani@gmx.at>
2015-04-26 15:34:31 +0200

    Signed-off-by: ThomasSpanberger <thomasspani@gmx.at>

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix calculates an inverted matrix to the supplied matrix, and 
+## caches both the supplied and the inverted matrix
+ 
+## This function calculates the inverted matrix using the solve command, then
+## caches both the initial matrix and the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-invertedmatrix <- solve(x)
-y <<- x
-cachedinversematrix <<- invertedmatrix
+invertedmatrix <- solve(x) 
+y <<- x 
+cachedinversematrix <<- invertedmatrix 
 invertedmatrix
 }
 
+## This function checks if 
+## (1) there is a cached initital matrix
+## (2) there is a cached inverse matrix
+## (3) the cached initial matrix is equal to the matrix given when the 
+## function is called
 
-## Write a short comment describing this function
+## if all of these facts are true, the cached matrix is called, otherwise, the 
+## inverse matrix is calculated
 
 cacheSolve <- function(x = matrix()) {
 if (!exists('y')) {
